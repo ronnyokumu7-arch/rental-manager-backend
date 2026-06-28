fef2efd1ef2e4695681da9ca9ef9dfbc0b0117c3
Ronny Okumu <ronnyokumu7@gmail.com>
2026-06-28 19:12:51 +0300

    feature: add subscription model

--- app/models/subscriptions.py
+++ app/models/subscriptions.py
@@ -51,10 +51,3 @@ class Subscription(Base):
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
     tenant = relationship("Tenant", back_populates="subscriptions")
-    invoices = relationship(
-    "Invoice", 
-    back_populates="subscription", 
-    cascade="all, delete-orphan",
-    primaryjoin="Subscription.id == Invoice.subscription_id",
-    foreign_keys="[Invoice.subscription_id]"
-)
